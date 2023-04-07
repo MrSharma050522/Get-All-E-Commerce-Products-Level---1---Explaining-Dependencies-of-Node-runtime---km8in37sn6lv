77d2b50545f613b1e1fa6969370a9cb0c1682c46
Sandeep Sharma <sandeepsharma10697@gmail.com>
2023-04-07 13:41:34 +0530

    done

--- src/app.js
+++ src/app.js
@@ -10,20 +10,9 @@ app.use(express.json());
 
 // Write GET endpoint for sending all the products to client here
 // Endpoint - /api/v1/products
-app.get("/api/v1/products/:name", (req, res) => {
-  const { name } = req.params;
+app.get("/api/v1/products", (req, res) => {
   if (products === null) {
     res.status(404).json({ message: "Product not found" });
-  }
-  let obj = null;
-  for (let i = 0; i < products.length; i++) {
-    if (products[i].name == name) {
-      obj = products[i];
-    }
-    // console.log(products[i], name);
-  }
-  if (obj == null) {
-    res.status(404).json({ status: "failed", message: "Product not found" });
   }
   res.status(200).json({
     status: "success",
